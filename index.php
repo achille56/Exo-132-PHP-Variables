1a24299a5a503cfa891dd6bf7c883b67a0b6302d
achille56 <leclercqjm59@gmail.com>
2023-01-10 13:36:43 +0100

    exo 132

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+    <?php
+        $texte = "Du texte est stocké";
+        echo $texte . "<br>";
+
+        $y = 30;
+        echo  "y vaux " . $y . "<br>";
+
+        $m =5;
+        $n =7;
+        echo $m + $n;
+    ?>
+</body>
+
+</html>
